d9c6974d0f3f2864ab3a595304a407866a2b86cb
shamshad144 <scubesystems14@gmail.com>
2023-06-12 11:53:23 +0530

    First commit

---
+++ src/main/java/com/s3cubesystems/common/S3CubeBankApplication.java
@@ -0,0 +1,13 @@
+package com.s3cubesystems.common;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S3CubeBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S3CubeBankApplication.class, args);
+	}
+
+}
